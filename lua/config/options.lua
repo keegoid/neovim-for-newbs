d739988acb28c81524621eb1f92e84a7a039dcd7
Keegan Mullaney <keegoid@pm.me>
2024-04-07 16:37:02 -0700

    add Lazy starter

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.opt.swapfile = false
+
+-- vim.wo.number = true
+
