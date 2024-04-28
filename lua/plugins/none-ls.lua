ace6eb160da48aa27715f87221102a96255cbac0
Keegan Mullaney <keegoid@pm.me>
2024-04-28 12:47:28 -0700

    add plugins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.diagnostics.markdownlint,
         null_ls.builtins.diagnostics.yamllint,
         null_ls.builtins.completion.spell,
