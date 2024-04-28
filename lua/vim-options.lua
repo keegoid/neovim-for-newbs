7b69d1214c6cf63cbd39ed43cc4cf9fd90b13573
Keegan Mullaney <keegoid@pm.me>
2024-04-28 13:14:31 -0700

    keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,11 +8,13 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>")
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
+
 vim.wo.number = true
 
