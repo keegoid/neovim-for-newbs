aef320b98b10281cada2b7ab3dc5829ca1183379
Keegan Mullaney <keegoid@pm.me>
2024-07-14 13:19:13 -0700

    Sun Jul 14 01:19:13 PM PDT 2024

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -27,10 +27,15 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>", { desc = "clear hlsearch" })
 vim.keymap.set("n", "<leader>lv", ":Lazy<CR>", { desc = "LazyVim" })
-vim.keymap.set("n", "<leader>lc", ":CheckHealth<CR>", { desc = "check health" })
+vim.keymap.set("n", "<leader>lc", ":checkhealth<CR>", { desc = "LazyVim healthcheck" })
 vim.keymap.set("n", "<leader>ww", ":call SaveAs()<CR>", { noremap = true, silent = true, desc = "window write" })
 vim.keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "window write and quit" })
-vim.keymap.set("n", "<leader>wQ", ":q!<CR>", { desc = "window quit!" })
+vim.keymap.set("n", "<leader>zz", ":q!<CR>", { desc = "window quit!" })
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+if vim.fn.executable('wl-copy') == 1 then
+  vim.keymap.set('v', '<leader>y', 'y:call system("wl-copy", getreg(\'"\'))<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>p', ':r !wl-paste<CR>', { noremap = true, silent = true })
+end
