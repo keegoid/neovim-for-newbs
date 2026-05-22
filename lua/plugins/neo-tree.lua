5df6288f87d925972a880fbfa0d83af46dc27a3c
Keegan Mullaney <kmullaney@newrelic.com>
2026-05-21 17:01:45 -0700

    fix: Alpha overlay

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,8 +7,8 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set("n", "<leader>ef", ":Neotree filesystem reveal toggle left<CR>", { desc = "explore file" })
-    vim.keymap.set("n", "<leader>eb", ":Neotree buffers reveal toggle float<CR>", { desc = "explore buffer" })
-    vim.keymap.set("n", "<leader>eg", ":Neotree git_status reveal toggle float<CR>", { desc = "explore git" })
+    vim.keymap.set("n", "<leader>ef", ":Neotree filesystem toggle left<CR>", { desc = "explore file" })
+    vim.keymap.set("n", "<leader>eb", ":Neotree buffers toggle float<CR>", { desc = "explore buffer" })
+    vim.keymap.set("n", "<leader>eg", ":Neotree git_status toggle float<CR>", { desc = "explore git" })
   end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,17 +2,18 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.markdownlint.with({
-          extra_args = { "--config", vim.fn.expand("~/.markdownlint.json") },
-        }),
-      },
-    })
+    local sources = {
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.formatting.stylua,
+    }
+    if vim.fn.executable("markdownlint") == 1 then
+      table.insert(sources, null_ls.builtins.diagnostics.markdownlint.with({
+        extra_args = { "--config", vim.fn.expand("~/.markdownlint.json") },
+      }))
+    end
+    null_ls.setup({ sources = sources })
 
     vim.keymap.set("n", "<leader>i", vim.lsp.buf.format, { desc = "format" })
   end,
